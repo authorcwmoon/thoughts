2647910bae61277549dc2103c4b462f1b1942649
Christian Williams <christian@MacBook-Pro.local>
2020-11-12 14:33:58 -0600

    all

--- src/pages/api/page-views.js
+++ src/pages/api/page-views.js
@@ -1,20 +1,20 @@
-import db from "../../lib/db-admin";
+// import db from "../../lib/db-admin";
 
-export default async function view(req, res) {
-  if (!req.query.id) {
-    return res.status(400).json({
-      error: 'Missing "id" query parameter',
-    });
-  }
+// export default async function view(req, res) {
+//   if (!req.query.id) {
+//     return res.status(400).json({
+//       error: 'Missing "id" query parameter',
+//     });
+//   }
 
-  const ref = db().ref("views").child(req.query.id);
-  const { snapshot } = await ref.transaction(currentViews => {
-    // if it has never been set it returns null
-    if (currentViews === null) currentViews = 0;
-    return currentViews + 1;
-  });
+//   const ref = db().ref("views").child(req.query.id);
+//   const { snapshot } = await ref.transaction(currentViews => {
+//     // if it has never been set it returns null
+//     if (currentViews === null) currentViews = 0;
+//     return currentViews + 1;
+//   });
 
-  res.status(200).json({
-    total: snapshot.val(),
-  });
-}
+//   res.status(200).json({
+//     total: snapshot.val(),
+//   });
+// }
